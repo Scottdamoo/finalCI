1c844f9f5f9fa93226b499773725c716627faad8
scottdamoo <scottdamoo@icloud.com>
2025-01-08 14:47:56 +0000

    loaded in dataset and imported libraries

--- final_prof.py
+++ final_prof.py
@@ -1,1 +1,9 @@
+import numpy as np 
+import matplotlib.pyplot as plt 
+import pandas as pd 
+import seaborn as sns 
 
+file_path = r'c:\Users\scott\OneDrive\Documents\YR2_UNIVERSITY\final prof\final.dataset.xlsx'
+
+gdp = pd.read_excel(file_path)
+print(gdp.head())  # Display the first 5 rows
